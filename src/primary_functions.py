90118140332ed5eed72ade9e0626dd99a131577d
Jay Jagpal <jay@strangemother.com>
2018-02-01 01:43:48 +0000

    Recursive population of word data + loop call

--- src/primary_functions.py
+++ src/primary_functions.py
@@ -12,8 +12,11 @@ import contextnet_api as cnapi
 from logger import print_edges
 import words
 import secondary_functions
+import loop
+
 
 ident_dict = {}
+
 
 def allow_external(bool=None):
     if bool is not None:
@@ -32,6 +35,8 @@ def init(socket_uri=None):
     basepath = os.path.abspath(os.path.dirname(__file__))
     cache_path = os.path.join(basepath, 'cache')
     set_global_root(cache_path)
+    loop.init(socket_uri=socket_uri, cache_path=cache_path)
+
 
 
 def assess_string(sentence):
@@ -39,21 +44,71 @@ def assess_string(sentence):
     Given an input string, token and assess each token.
     '''
     # success, v = get_cache(filepath)
+    thin_structure_words = tokenize_assess_string(sentence, allow_fetch=True)
+    # Send to the data cleaner for the next stage of cross referencing.
+    populate_sub_words(thin_structure_words)
+    secondary_functions.apply_to_context(thin_structure_words)
+    return thin_structure_words
+
+def populate_sub_words(thin_structure_words, allow_fetch=True):
+    '''A parent string has been tokenized and filled with the raw
+    cache data of the words. Populate the same for any
+    sub words, attached synonyms and antoynms
+    '''
+    words = thin_structure_words
+    print 'tree',  thin_structure_words['tree']
+    result = []
+
+    if 'words' in words:
+
+        for wordset in words['words']:
+            word, wtype, ident, dict_ref = wordset
+            # populate similar
+            subl = dict_ref['synonym']
+            if subl is None:
+                continue
+            print 'digressing synonyms of', word, len(subl)
+            for sub_word in subl:
+                print "looking at - ", sub_word
+                value = tokenize_assess_string(sub_word.lower(), allow_fetch=allow_fetch)
+                result.append({ 'value': sub_word, 'meta': value })
+    else:
+        print 'no word information'
+
+    return result
+
+def populate_sub_words_recursive(thin_structure_words, depth=6, _current_depth=0, root_value=None):
+    print 'populate sub words recursive {}/{}'.format(_current_depth, depth)
+    words = populate_sub_words(thin_structure_words, allow_fetch=False)
+    if _current_depth >= depth:
+        print 'kill depth'
+        return words
+
+    for word_result in words:
+        if 'words' in word_result['meta']:
+            print 'recursive inspection of', len(word_result['meta'])
+            _words = populate_sub_words_recursive(
+                word_result['meta'],
+                depth=depth,
+                _current_depth=_current_depth+1,
+                root_value=root_value,
+                )
+            words.extend(_words)
+
+
+
+def ident_object(word, wtype, tokens):
+    return dict()
+
+def tokenize_assess_string(sentence, allow_fetch=True):
+     # success, v = get_cache(filepath)
     v = cache.get_string_cache(sentence)
 
     if v is None:
         v = tokenize(sentence)
         cache.set_string_cache(sentence, v)
 
-    thin_structure_words = assess_tokens(v)
-    # Send to the data cleaner for the next stage of cross referencing.
-    secondary_functions.apply_to_context(thin_structure_words)
-    return thin_structure_words
-
-
-def ident_object(word, wtype, tokens):
-    return dict()
-
+    return assess_tokens(v, allow_fetch=allow_fetch)
 
 def iter_print(g):
     print '\n'
@@ -77,7 +132,7 @@ def tokenize(s):
     return g
 
 
-def assess_tokens(tokens):
+def assess_tokens(tokens, allow_fetch=True):
     '''
     Read and analyze a list of tokens. For each discovered work, entire
     word assertions exist.
@@ -100,7 +155,7 @@ def assess_tokens(tokens):
 
     for word, wtype in tokens:
         # thin_structure_words['words'][3] == tokens
-        ident = assess_word_token(word, wtype, tokens)
+        ident = assess_word_token(word, wtype, tokens, allow_fetch=allow_fetch)
         wdd = words.get_word(word)
         res = res + ( (word, wtype, ident, wdd), )
         send_json(
@@ -116,7 +171,7 @@ def assess_tokens(tokens):
     return dict(tree=tree, words=res)
 
 
-def assess_word_token(word, wtype, tokens):
+def assess_word_token(word, wtype, tokens, allow_fetch=True):
     '''
     Given a Word "cake", its type "NN" and associated sentence tokens, "I like cake"
     identify the word store a cache of identification into the ident_dict.
@@ -132,7 +187,7 @@ def assess_word_token(word, wtype, tokens):
 
     if ident_dict.get(word, None) is None:
         # add to flat
-        ident_dict[word] = ident_parent_object(word, wtype, ident, tokens)
+        ident_dict[word] = ident_parent_object(word, wtype, ident, tokens, allow_fetch=allow_fetch)
 
     ident_dict[word]['idents'].add(ident)
     ident_dict[word]['words'].add(word)
@@ -141,7 +196,7 @@ def assess_word_token(word, wtype, tokens):
     return ident_dict[word]
 
 
-def ident_parent_object(word, wtype, ident, tokens):
+def ident_parent_object(word, wtype, ident, tokens, allow_fetch=True):
     '''
     Create a single word entry to the identity context, associating any
     _wtype additions
@@ -154,7 +209,7 @@ def ident_parent_object(word, wtype, ident, tokens):
     The meta data within the result will call the to the context API.
     '''
     res = dict(words=set(), wtypes=set(), idents=set())
-    res['meta'] = cnapi.api_result(word.lower())
+    res['meta'] = cnapi.api_result(word.lower(), allow_fetch=allow_fetch)
     #    {'end': {u'@id': u'/c/en/greeting',
     #         u'label': u'greeting',
     #         u'language': u'en',
